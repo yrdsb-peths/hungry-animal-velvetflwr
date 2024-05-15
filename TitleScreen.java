f28bfcab086a3315493105013f23c365ad50ab79
michelle <341078855@gapps.yrdsb.ca>
2024-05-14 21:52:36 -0400

    titel screen added

---
+++ TitleScreen.java
@@ -0,0 +1,52 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Hungry Elephant", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+
+        addObject(titleLabel, getWidth()/2, 100);
+        prepare();
+    }
+
+    /**
+     * The main world act loop
+     */
+    public void act()
+    {
+        //Start the game if user presses the space bar
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,442,123);
+        elephant.setLocation(306,284);
+        Label subLabel = new Label("Press space to begin!", 40);
+        addObject(subLabel, getWidth()/2, 175);
+        Label instruct = new Label("Use \u2190 and \u2192 to move!", 30);
+        addObject(subLabel, getWidth()/2, 185);
+    }
+}
